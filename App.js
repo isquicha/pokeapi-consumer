e9e8b918b9548e9e584c854446ebae28c4dadcd0
Andre Vicente <isquicha@gmail.com>
2021-03-31 13:18:25 -0300

    feat: Add initial app

--- App.js
+++ App.js
@@ -1,21 +1,104 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {
+  View,
+  Image,
+  Text,
+  SafeAreaView,
+  FlatList,
+  StyleSheet,
+  StatusBar
+} from 'react-native';
 
-export default function App() {
+
+class App extends Component {
+  state = { pokemons: [] };
+
+  async componentDidMount() {
+    const response = await fetch(
+      'https://pokeapi.co/api/v2/pokemon',
+      {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json'
+        }
+      }
+    );
+    const json = await response.json();
+    this.setState({ pokemons: json.results });
+  }
+
+
+  render() {
+    return (
+      <SafeAreaView style={styles.main}>
+        <StatusBar hidden={true} />
+        <Text style={styles.title}>Pokemons</Text>
+        <FlatList
+          data={this.state.pokemons}
+          keyExtractor={(pokemon) => pokemon.name}
+          contentContainerStyle={styles.pokemonList}
+          renderItem={showPokemon}
+        />
+      </SafeAreaView>
+    );
+  }
+};
+
+
+const showPokemon = (pokemon) => {
+
+  const { name, url } = pokemon.item;
+  const pokemonNumber = url.replace(
+    'https://pokeapi.co/api/v2/pokemon/', ''
+  ).replace('/', '');
+  const imageUrl = 'https://pokeres.bastionbot.org/images/pokemon/' + pokemonNumber + '.png';
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.pokemon}>
+      <Image
+        style={styles.pokemonImage}
+        source={{ uri: imageUrl }}
+      />
+      <Text style={styles.pokemonName}>{capitalize(name)}</Text>
     </View>
   );
-}
+};
+
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  // Main
+  main: {
+    backgroundColor: "#163643",
   },
+  title: {
+    fontSize: 40,
+    paddingBottom: 10,
+    fontFamily: "Roboto",
+    fontWeight: "800",
+    alignSelf: "center",
+    color: "#f4a261",
+  },
+  // Pokemon
+  pokemonList: {
+    flexGrow: 1,
+    alignItems: 'center',
+    backgroundColor: "#264653",
+    paddingBottom: 70,
+  },
+  pokemon: {
+    flexDirection: 'column',
+  },
+  pokemonName: {
+    color: "#e9c46a",
+    fontSize: 20,
+  },
+  pokemonImage: {
+    width: 70, height: 70,
+  },
+
+
 });
+
+const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
+
+
+export default App;
